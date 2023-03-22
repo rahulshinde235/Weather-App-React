c6b3bde1997d55be3dd60843660e4a84f38f0dc1
rahulshinde235 <rahulshinde235@gmail.com>
2023-03-23 00:05:16 +0530

    Updated http to https

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
       setError(false);
       try {
         let res = await fetch(
-          `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=2&appid=${
+          `https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=2&appid=${
             import.meta.env.VITE_APP_API_KEY
           }`
         );
